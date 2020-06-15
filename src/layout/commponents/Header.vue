aa827169799056f289d6f1e05a53747d96fc8af9
showxuanzi <15369302901@163.com>
2020-06-15 22:29:02 +0800

    面包屑样式

--- src/layout/commponents/Header.vue
+++ src/layout/commponents/Header.vue
@@ -1,24 +1,35 @@
 <template>
     <div>
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
       <el-button type="text" @click="goback">退出</el-button>
     </div>
 </template>
 <script>
 import {logout} from "@/api/article";
-import {reomveToken} from "@/utils/token";
+import {getToken,reomveToken} from "@/utils/token";
 export default {
   name: "layout-header",
   methods:{
     goback(){
-      console.log("tuichu");
-      logout()
-      .then(res => {
-        if(res.data.code === 20000){
-          reomveToken();
-          this.$router.push("/login");
-        }
-      })
-    }
+      let token = getToken();
+      if(token){
+        logout().then(res => {
+          if(res.data.code === 20000){
+            reomveToken();
+            this.$router.push("/login");
+          }
+        })
+      }else{
+        reomveToken();
+        this.$router.push("/login");
+      }
+    },
+    
   }
 }
 </script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
     <div class="box">
         <PanelProup />
         <el-row :gutter= "30" class="panel-group">
-            <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" class="">
+            <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
                 <Map />
             </el-col>
             <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
